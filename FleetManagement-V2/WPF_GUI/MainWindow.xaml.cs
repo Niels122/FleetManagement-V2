49ad6a8a4a1556503e0dd6bfeb5728fb06a5b7a5
NielsVM <niels.vanmaelzaeke@gmail.com>
2022-05-12 14:30:44 +0200

    Bestuurder window toegevoegd

--- FleetManagement-V2/WPF_GUI/MainWindow.xaml.cs
+++ FleetManagement-V2/WPF_GUI/MainWindow.xaml.cs
@@ -22,40 +22,5 @@ namespace WPF_GUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        private DomeinController _dc;
-        public MainWindow(DomeinController dc)
-        {
-            _dc = dc;
-            InitializeComponent();
-            RefreshBestuurders();
-        }
-
-        private void RefreshBestuurders()
-        {
-            List<Bestuurder> bestuurderView = new();
-            List<List<String>> bestuurdersInfo = _dc.GeefBestuurders();
-
-            foreach (List<String> bestuurderInfo in bestuurdersInfo)
-            {
-               // bestuurderView.Add(new BestuurderView(bestuurderInfo[0], bestuurderInfo[1], bestuurderInfo[2]));
-            }
-
-            LVOverzichtBestuurder.ItemsSource = bestuurderView;
-        }
-
-    }
-    public class BestuurderView
-    {
-        public string Naam { get; set; }
-        public string Voornaam { get; set; }
-        public string Rijksregisternummer { get; set; }
-
-        public BestuurderView(string naam, string voornaam, string rijksregisternummer)
-        {
-            Naam = naam;
-            Voornaam = voornaam;    
-            Rijksregisternummer = rijksregisternummer;
-            
-        }
     }
 }
