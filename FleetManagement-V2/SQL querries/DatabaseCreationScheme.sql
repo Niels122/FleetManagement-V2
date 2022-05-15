c678b8558bd527689e9287e4f334f60d76ba3962
Niels Van Maelzaeke <niels.vanmaelzaeke@gmail.com>
2022-05-15 19:20:21 +0200

    SQL aangepast: bestuurde aanmaak onderaan geplaatst

--- FleetManagement-V2/SQL querries/DatabaseCreationScheme.sql
+++ FleetManagement-V2/SQL querries/DatabaseCreationScheme.sql
@@ -1,17 +1,3 @@
---DROP TABLE bestuurder;
-CREATE TABLE bestuurder (
-	id INT IDENTITY(1,1) PRIMARY KEY,
-	naam VARCHAR(45) NULL,
-	voornaam VARCHAR(45) NULL,
-	geboortedatum DATE NOT NULL,
-	rijksregisternummer VARCHAR(11) NOT NULL UNIQUE,
-	rijbewijstype VARCHAR(45) NULL,
-	voertuigId INT NULL UNIQUE,
-	tankkaartId INT NULL UNIQUE,
-	adresId INT NOT NULL,
-)
-GO
-
 --DROP TABLE tankkaart;
 CREATE TABLE tankkaart(
 	id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
@@ -33,7 +19,7 @@ CREATE TABLE voertuig (
 	typevoertuig VARCHAR(45) NULL,
 	brandstof VARCHAR(45) NULL,
 	kleur VARCHAR(45) NULL,
-	aantalDeuren VARCHAR(45) NULL,
+	aantalDeuren int NULL,
 )
 GO
 
@@ -44,6 +30,20 @@ CREATE TABLE adres(
 	huisnummer VARCHAR(10) NOT NULL,
 	postcode INT NOT NULL,
 	stad VARCHAR(255) NOT NULL,
+)
+GO
+
+--DROP TABLE bestuurder;
+CREATE TABLE bestuurder (
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	naam VARCHAR(45) NULL,
+	voornaam VARCHAR(45) NULL,
+	geboortedatum DATE NOT NULL,
+	rijksregisternummer VARCHAR(11) NOT NULL UNIQUE,
+	rijbewijstype VARCHAR(45) NULL,
+	voertuigId INT NULL UNIQUE,
+	tankkaartId INT NULL UNIQUE,
+	adresId INT NOT NULL,
 )
 GO
 
