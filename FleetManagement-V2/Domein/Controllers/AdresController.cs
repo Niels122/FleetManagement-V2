5b6f2887670f2891a538a7d9e69d8303f81f2748
BenedekSajgo <benedeksajgo@gmail.com>
2022-05-19 00:49:18 +0200

    Adres toegevoegd aan Domeinlaag

---
+++ FleetManagement-V2/Domein/Controllers/AdresController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domein.Interfaces;
+using Domein.Objects;
+
+
+namespace Domein.Controllers
+{
+    public class AdresController
+    {
+        private IAdresRepository _adresRepo;
+
+        public AdresController(IAdresRepository adresRepo)
+        {
+            _adresRepo = adresRepo;
+        }
+
+        public List<Adres> GeefAdressen()
+        {
+            return _adresRepo.GeefAdressen();
+        }
+    }
+}
---
+++ FleetManagement-V2/Domein/Interfaces/IAdresRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domein.Objects;
+
+namespace Domein.Interfaces
+{
+    public interface IAdresRepository
+    {
+        List<Adres> GeefAdressen();
+    }
+}
