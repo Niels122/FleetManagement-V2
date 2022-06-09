5439b6afe06fd5c20599ec573daf9bb400ebfe80
Niels Van Maelzaeke <niels.vanmaelzaeke@gmail.com>
2022-06-09 17:12:37 +0200

    DATABASE update

--- FleetManagement-V2/SQL querries/DatabaseCreationScheme.sql
+++ FleetManagement-V2/SQL querries/DatabaseCreationScheme.sql
@@ -1,29 +1,43 @@
+--DROP DATABASE FleetmanagementDB
+
+
 --DROP TABLE tankkaart;
+--DROP TABLE adres;
+--DROP TABLE bestuurder;
+--DROP TABLE voertuig;
+--GO
+
+--CREATE DATABASE FleetmanagementDB
+
 CREATE TABLE tankkaart(
-	id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	tankkaartnummer VARCHAR(45) NOT NULL UNIQUE,
+	tankkaartnummer VARCHAR(18) PRIMARY KEY,
 	geldigheidsDatum DATE NOT NULL,
-	brandstof VARCHAR(45) NULL,
+	brandstof VARCHAR(45) NULL, 
 	pincode INT NULL,
-	isActief BIT NOT NULL,
+	isGeblokkeerd BIT NOT NULL,
+	isDeleted BIT NOT NULL DEFAULT 0,
 )
+
 GO
 
---DROP TABLE voertuig;
+
 CREATE TABLE voertuig (
-	id INT IDENTITY(1,1) PRIMARY KEY,
-	nummerplaat VARCHAR(45) NOT NULL UNIQUE,
-	chassisnummer VARCHAR(45) NOT NULL UNIQUE,
-	merk VARCHAR(45) NULL,
-	model VARCHAR(45) NULL,
-	typevoertuig VARCHAR(45) NULL,
-	brandstof VARCHAR(45) NULL,
+
+	chassisnummer VARCHAR(17) PRIMARY KEY,
+	nummerplaat VARCHAR(45) NOT NULL,
+	merk VARCHAR(45) NOT NULL,
+	model VARCHAR(45) NOT NULL,
+	typevoertuig VARCHAR(45) NOT NULL,
+	brandstof VARCHAR(45) NOT NULL,
 	kleur VARCHAR(45) NULL,
 	aantalDeuren int NULL,
+	isDeleted BIT NOT NULL DEFAULT 0,
+
+	CONSTRAINT UC_Nummerplaat UNIQUE (nummerplaat),
 )
+	
 GO
 
---DROP TABLE adres;
 CREATE TABLE adres(
 	id INT IDENTITY(1,1) PRIMARY KEY,
 	straat VARCHAR(255) NOT NULL,
@@ -33,22 +47,35 @@ CREATE TABLE adres(
 )
 GO
 
---DROP TABLE bestuurder;
 CREATE TABLE bestuurder (
-	id INT IDENTITY(1,1) PRIMARY KEY,
-	naam VARCHAR(45) NULL,
-	voornaam VARCHAR(45) NULL,
+	id VARCHAR(8) PRIMARY KEY,
+	naam VARCHAR(45) NOT NULL,
+	voornaam VARCHAR(45) NOT NULL,
 	geboortedatum DATE NOT NULL,
-	rijksregisternummer VARCHAR(11) NOT NULL UNIQUE,
-	rijbewijstype VARCHAR(45) NULL,
-	voertuigId INT NULL UNIQUE,
-	tankkaartId INT NULL UNIQUE,
-	adresId INT NOT NULL,
+	rijksregisternummer VARCHAR(11) NOT NULL,
+	rijbewijstype VARCHAR(15) NOT NULL,
+	chassisnummerVoertuig VARCHAR(17) NULL,
+	tankkaartnummer VARCHAR(18) NULL,
+	adresId INT NULL,
+	isDeleted BIT NOT NULL DEFAULT 0,
+
+	CONSTRAINT UC_Rijksregisternummer UNIQUE (rijksregisternummer),
+	--CONSTRAINT UC_VoertuigId UNIQUE (voertuigId),
+	--CONSTRAINT UC_TankkaartId UNIQUE (tankkaartId),
+
 )
+
+	CREATE UNIQUE INDEX UI_ChassisnummerVoertuig
+	ON bestuurder(chassisnummerVoertuig)
+	WHERE chassisnummerVoertuig IS NOT NULL
+
+	CREATE UNIQUE INDEX UI_Tankkaartnummer
+	ON bestuurder(tankkaartnummer)
+	WHERE tankkaartnummer IS NOT NULL
 GO
 
 
 
-ALTER TABLE bestuurder ADD CONSTRAINT FK_AdresIdBestuurder FOREIGN KEY (adresId) REFERENCES adres(id);
-ALTER TABLE bestuurder ADD CONSTRAINT FK_VoertuigIdBestuurder FOREIGN KEY (voertuigId) REFERENCES voertuig(id);
-ALTER TABLE bestuurder ADD CONSTRAINT FK_TankkaartIdBestuurder FOREIGN KEY (tankkaartId) REFERENCES tankkaart(id);
+ALTER TABLE bestuurder ADD CONSTRAINT FK_AdresBestuurder FOREIGN KEY (adresId) REFERENCES adres(id);
+ALTER TABLE bestuurder ADD CONSTRAINT FK_VoertuigBestuurder FOREIGN KEY (chassisnummerVoertuig) REFERENCES voertuig(chassisnummer);
+ALTER TABLE bestuurder ADD CONSTRAINT FK_TankkaartBestuurder FOREIGN KEY (tankkaartnummer) REFERENCES tankkaart(tankkaartnummer);
--- FleetManagement-V2/SQL querries/DatabaseFill.sql
+++ FleetManagement-V2/SQL querries/DatabaseFill.sql
@@ -7,64 +7,49 @@
 --GO
 
 
-INSERT INTO bestuurder
-      ([naam]
-      ,[voornaam]
-      ,[geboortedatum]
-      ,[rijksregisternummer]
-      ,[rijbewijstype]
-      ,[voertuigId]
-      ,[tankkaartId]
-      ,[adresId])
+	INSERT INTO tankkaart
 
-	  VALUES 
-	  ('Van Maelzaeke','Niels','1999-12-07', '99120750392','B',1,1,1),
-	  ('Sajgo','Benedek','1996-07-15', '96071800169','B',2,2,2),
-	  ('Vos','Jan','1958-06-10', '58061000186','C',3,3,3),
-	  ('Varen','Bart','2002-02-02', '02020200124','B',4,4,4),
-	  ('Van Wal','Lena','1986-05-09', '86050900217','C',5,5,5)
+		([tankkaartnummer]
+		,[geldigheidsDatum]
+		,[brandstof]
+		,[pincode]
+		,[isGeblokkeerd])
 
+	  VALUES
+	  ('123456789', '2024-12-31', 'benzine'		, 1234, 0),
+	  ('987654321', '2024-12-31', 'benzine'		, 1111, 0),
+	  ('123654789', '2024-12-31', 'diesel'		, 4321, 0),
+	  ('321456987', '2022-10-31', 'benzine'		, 3456, 0),
+	  ('111222333', '2022-10-31', 'elektrisch'	, 6789, 0)
+	 
 
   INSERT INTO voertuig
-		([nummerplaat]
-      ,[chassisnummer]
-      ,[merk]
-      ,[model]
-      ,[typevoertuig]
-      ,[brandstof]
-      ,[kleur]
-      ,[aantalDeuren])
+
+		([chassisnummer]
+		,[nummerplaat]
+		,[merk]
+		,[model]
+		,[typevoertuig]
+		,[brandstof]
+		,[kleur]
+		,[aantalDeuren])
 
 	  VALUES
-	  ('AAA-111', 'WVWND23B5YE381216', 'mercedes'	, 'cls'	, 'personenauto', 'benzine'		, 'grijs'	, '5'),
-	  ('BBB-222', '1FT8W3BTXCEB73445', 'volkswagen'	, 'polo', 'personenauto', 'benzine'		, 'blauw'	, '3'),
-	  ('CCC-333', 'JM1BK32F581189675', 'audi'		, 'a3'	, 'bestelwagen'	, 'diesel'		, 'zwart'	, '5'),
-	  ('DDD-444', '5TDZK22C39S270189', 'volkswagen'	, 'golf', 'personenauto', 'benzine'		, 'geel'	, '5'),
-	  ('EEE-555', '1G2ZM151764127062', 'audi'		, 'q8'	, 'bestelwagen'	, 'elektrisch'	, 'groen'	, '5')
+	  ('WVWND23B5YE381216', 'AAA-111', 'mercedes'	, 'cls'	, 'personenauto', 'benzine'		, 'grijs'	, '5'),
+	  ('1FT8W3BTXCEB73445',	'BBB-222', 'volkswagen'	, 'polo', 'personenauto', 'benzine'		, 'blauw'	, '3'),
+	  ('JM1BK32F581189675', 'CCC-333', 'audi'		, 'a3'	, 'bestelwagen'	, 'diesel'		, 'zwart'	, '5'),
+	  ('5TDZK22C39S270189',	'DDD-444', 'volkswagen'	, 'golf', 'personenauto', 'benzine'		, 'geel'	, '5'),
+	  ('1G2ZM151764127062',	'EEE-555', 'audi'		, 'q8'	, 'bestelwagen'	, 'elektrisch'	, 'groen'	, '5')
 
-
-	INSERT INTO tankkaart
-		([tankkaartnummer]
-      ,[geldigheidsDatum]
-      ,[brandstof]
-      ,[pincode]
-      ,[isActief])
-
-	  VALUES
-	  ('123456789', '2024-12-31', 'benzine'		, 1234, 1),
-	  ('987654321', '2024-12-31', 'benzine'		, 1111, 1),
-	  ('123654789', '2024-12-31', 'diesel'		, 4321, 1),
-	  ('321456987', '2022-10-31', 'benzine'		, 3456, 1),
-	  ('111222333', '2022-10-31', 'elektrisch'	, 6789, 1)
-	 
 
 
 
 	INSERT INTO adres
-	(  [straat]
-      ,[huisnummer]
-      ,[postcode]
-      ,[stad])
+
+		([straat]
+		,[huisnummer]
+		,[postcode]
+		,[stad])
 
 	  VALUES
 	  ('Pelikaanstraat' , '122'	, 9700	, 'Oudenaarde'),
@@ -72,3 +57,29 @@ INSERT INTO bestuurder
 	  ('Vogelstraat'	, '7a'	, 9000	, 'Gent'),
 	  ('Kergate'		, '65'	, 9800	, 'Astene'),
 	  ('Weedreef'		, '1'	, 8700	, 'Tielt')
+
+
+INSERT INTO bestuurder
+
+      ([id]
+	  ,[naam]
+      ,[voornaam]
+      ,[geboortedatum]
+      ,[rijksregisternummer]
+      ,[rijbewijstype]
+      ,[tankkaartnummer]
+      ,[chassisnummerVoertuig]
+      ,[adresId])
+
+	  VALUES 
+	  ('111111nv'	,'Van Maelzaeke'	,'Niels'	,'1999-12-07', '99120750392','B','123456789', 'WVWND23B5YE381216', 1),
+	  ('222222bs'	,'Sajgo'			,'Benedek'	,'1996-07-15', '96071800169','B','987654321', '1FT8W3BTXCEB73445', 2),
+	  ('333333jv'	,'Vos'				,'Jan'		,'1958-06-10', '58061000186','C','123654789', 'JM1BK32F581189675', 3),
+	  ('444444bv'	,'Varen'			,'Bart'		,'2002-02-02', '02020200124','B','321456987', '5TDZK22C39S270189', 4),
+	  ('555555vl'	,'Van Wal'			,'Lena'		,'1986-05-09', '86050900217','C','111222333', '1G2ZM151764127062', 5)
+
+	  
+
+
+	 
+
