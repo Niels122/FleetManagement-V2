d7fd25d7d304e89256b2bb0828698d4ae4f08fec
Niels Van Maelzaeke <niels.vanmaelzaeke@gmail.com>
2022-06-10 16:02:43 +0200

    Aanpassing zodat brandstoftype als string in databank komt

--- FleetManagement-V2/Domein/Interfaces/ITankkaartRepository.cs
+++ FleetManagement-V2/Domein/Interfaces/ITankkaartRepository.cs
@@ -10,11 +10,11 @@ namespace Domein.Interfaces
     public interface ITankkaartRepository
     {
         List<Tankkaart> GeefTankkaarten();
-        List<int> GeefTankkaartnummers();
+        //List<int> GeefTankkaartnummers();
 
         #region CRUD operations
         void CreateTankkaart(Tankkaart tankkaart);
-        Tankkaart ReadTankkaart(Tankkaart tankkaart);
+        //Tankkaart ReadTankkaart(Tankkaart tankkaart);
         void UpdateTankkaart(Tankkaart tankkaart);
         void DeleteTankkaart(Tankkaart tankkaart);
         #endregion
