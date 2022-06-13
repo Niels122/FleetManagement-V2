b132388f907b6b982605e18f6ddc46d0ee65a750
BenedekSajgo <benedeksajgo@gmail.com>
2022-06-14 01:33:23 +0200

    Persistentielaag helemaal afgwerkt
    - VoertuigRepository geimplementeerd
    - TankkaartRepository geimplementeerd
    - Werkwijze met enums vereenvoudigd

--- FleetManagement-V2/Persistentie/VoertuigRepository.cs
+++ FleetManagement-V2/Persistentie/VoertuigRepository.cs
@@ -14,30 +14,6 @@ namespace Persistentie
     public class VoertuigRepository : IVoertuigRepository
     {
         private const string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog = FleetmanagementDB; Integrated Security = True; TrustServerCertificate=True";
-        //TODO: implement interface
-        public void CreateVoertuig(Voertuig voertuig)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DeleteVoertuig(Voertuig voertuig)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<int> GeefChassisnummers()
-        {
-            List<int> chassisnummers = new List<int>();
-
-            return chassisnummers;
-        }
-
-        public List<string> GeefNummerplaten()
-        {
-            List<string> nummerplaten = new List<string>();
-
-            return nummerplaten;
-        }
 
         public List<Voertuig> GeefVoertuigen()
         {
@@ -48,65 +24,26 @@ namespace Persistentie
                 {
                     conn.Open();
 
-                    string readSql = "SELECT chassisnummer, nummerplaat, merk, model, typevoertuig, " +
-                        "brandstof, kleur, aantalDeuren FROM voertuig WHERE isDeleted = 0";
+                    string readSql = "SELECT chassisnummer, nummerplaat, merk, model, typevoertuig, brandstof, " +
+                        "kleur, aantalDeuren FROM voertuig WHERE isDeleted = 0";
                     SqlCommand cmd = new(readSql, conn);
 
-                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    using (SqlDataReader dataReader = cmd.ExecuteReader())
                     {
-                        if (reader.HasRows)
+                        if (dataReader.HasRows)
                         {
-                            while (reader.Read())
+                            while (dataReader.Read())
                             {
-                                string chassisnummer = (string)reader["chassisnummer"];
-                                string nummerplaat = (string)reader["nummerplaat"];
-                                string merk = (string)reader["merk"];
-                                string model = (string)reader["model"];
-                                string wagentype = (string)reader["typevoertuig"];
-                                string brandstof = (string)reader["brandstof"];
-                                string kleur = (string)reader["kleur"];
-                                int aantaldeuren = (int)reader["aantaldeuren"];
+                                string chassisnummer = (string)dataReader["chassisnummer"];
+                                string nummerplaat = (string)dataReader["nummerplaat"];
+                                string merk = (string)dataReader["merk"];
+                                string model = (string)dataReader["model"];
+                                Wagentype wagentype = (Wagentype)Enum.Parse(typeof(Wagentype),(string)dataReader["typevoertuig"]);
+                                Brandstoftype brandstoftype = (Brandstoftype)Enum.Parse(typeof(Brandstoftype), (string)dataReader["brandstof"]);
+                                string kleur = Convert.IsDBNull(dataReader["kleur"]) ? null : (string)dataReader["kleur"];
+                                int? aantaldeuren = Convert.IsDBNull(dataReader["aantalDeuren"]) ? null : (int?)dataReader["aantalDeuren"];
 
-                               #region setBrandstoftype
-                                    //tabel in database met brandstoftype en id + foreign key of switch gebruiken
-                                    //dit moet gebeuren omdat brandstoftype een enum is. Misschien is hier een mooiere oplossing voor maar dit is het enige wat ik zelf kon bedenken.
-                                    Brandstoftype brandstoftype;
-                                    if (brandstof == "elektrisch")
-                                    {
-                                        brandstoftype = Brandstoftype.elektrisch;
-                                    }
-                                    else if (brandstof == "diesel")
-                                    {
-                                        brandstoftype = Brandstoftype.diesel;
-                                    }
-                                    else if (brandstof == "hybridebenzine")
-                                    {
-                                        brandstoftype = Brandstoftype.hybrideBenzine;
-                                    }
-                                    else if (brandstof == "hybridediesel")
-                                    {
-                                        brandstoftype = Brandstoftype.hybrideDiesel;
-                                    }                               
-                                    else
-                                    {
-                                        brandstoftype = Brandstoftype.benzine;
-                                    }
-                                    #endregion
-
-                               #region setWagentype
-                                    //van string naar enum.
-                                    Wagentype _wagentype;
-                                    if (wagentype == "personenauto")
-                                    {
-                                        _wagentype = Wagentype.personenauto;
-                                    }
-                                    else
-                                    {
-                                        _wagentype = Wagentype.bestelwagen;
-                                    }
-                                    #endregion
-
-                                voertuigen.Add(new Voertuig(merk, model, chassisnummer, nummerplaat, brandstoftype, _wagentype, kleur, aantaldeuren, null));
+                                voertuigen.Add(new Voertuig(merk, model, chassisnummer, nummerplaat, brandstoftype, wagentype, kleur, aantaldeuren, null));
                             }
                         }
                     }
@@ -114,20 +51,154 @@ namespace Persistentie
             }
             catch (Exception ex)
             {
-                throw new VoertuigException($"Er gign iets mis in de persitentielaag bij het ophalen van de Voertuigen {ex.Message}"); 
+                throw new VoertuigException($"Er ging iets mis in de persitentielaag bij het ophalen van de voertuigen, {ex.Message}"); 
             }
 
             return voertuigen;
         }
 
-        public Voertuig ReadVoertuig()
+        public void CreateVoertuig(Voertuig voertuig)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string insertSql = "INSERT INTO voertuig (chassisnummer, nummerplaat, merk, model, typevoertuig, brandstof, kleur, aantalDeuren)" +
+                        "VALUES (@Chassisnummer, @Nummerplaat, @Merk, @Model, @Wagentype, @Brandstoftype, @Kleur, @AantalDeuren)";
+                    SqlCommand insertCommand = new(insertSql, conn);
+
+                    insertCommand.Parameters.AddWithValue("@Chassisnummer", voertuig.Chassisnummer);
+                    insertCommand.Parameters.AddWithValue("@Nummerplaat", voertuig.Nummerplaat);
+                    insertCommand.Parameters.AddWithValue("@Merk", voertuig.Merk);
+                    insertCommand.Parameters.AddWithValue("@Model", voertuig.Model);
+                    insertCommand.Parameters.AddWithValue("@Wagentype", voertuig.Wagentype.ToString());
+                    insertCommand.Parameters.AddWithValue("@Brandstoftype", voertuig.Brandstoftype.ToString());
+                    if (voertuig.Kleur == null)
+                    {
+                        insertCommand.Parameters.AddWithValue("@Kleur", DBNull.Value);
+                    }
+                    else
+                    {
+                        insertCommand.Parameters.AddWithValue("@Kleur", voertuig.Kleur);
+                    }
+                    if (voertuig.AantalDeuren == null)
+                    {
+                        insertCommand.Parameters.AddWithValue("@AantalDeuren", DBNull.Value);
+                    }
+                    else
+                    {
+                        insertCommand.Parameters.AddWithValue("@AantalDeuren", voertuig.AantalDeuren);
+                    }
+
+                    insertCommand.ExecuteNonQuery();
+
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new VoertuigException($"Er ging iets mis in de persitentielaag bij het creeren van het voertuig, { ex.Message }");
+            }
         }
 
-        public void UpdateVoertuig(Voertuig oudVoertuig, Voertuig nieuwVoertuig)
+        public void UpdateVoertuig(Voertuig voertuig)
+
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string updateSql = "UPDATE voertuig SET chassisnummer = @Chassisnummer, nummerplaat = @Nummerplaat, merk = @Merk, " +
+                        "model = @Model, typevoertuig = @Wagentype, brandstof = @Brandstoftype, kleur = @Kleur, aantalDeuren = @AantalDeuren " +
+                        "WHERE chassisnummer = @Chassisnummer";
+                    SqlCommand updateCommand = new(updateSql, conn);
+
+                    updateCommand.Parameters.AddWithValue("@Chassisnummer", voertuig.Chassisnummer);
+                    updateCommand.Parameters.AddWithValue("@Nummerplaat", voertuig.Nummerplaat);
+                    updateCommand.Parameters.AddWithValue("@Merk", voertuig.Merk);
+                    updateCommand.Parameters.AddWithValue("@Model", voertuig.Model);
+                    updateCommand.Parameters.AddWithValue("@Wagentype", voertuig.Wagentype.ToString());
+                    updateCommand.Parameters.AddWithValue("@Brandstoftype", voertuig.Brandstoftype.ToString());
+                    if (voertuig.Kleur == null)
+                    {
+                        updateCommand.Parameters.AddWithValue("@Kleur", DBNull.Value);
+                    }
+                    else
+                    {
+                        updateCommand.Parameters.AddWithValue("@Kleur", voertuig.Kleur);
+                    }
+                    if (voertuig.AantalDeuren == null)
+                    {
+                        updateCommand.Parameters.AddWithValue("@AantalDeuren", DBNull.Value);
+                    }
+                    else
+                    {
+                        updateCommand.Parameters.AddWithValue("@AantalDeuren", voertuig.AantalDeuren);
+                    }
+
+                    updateCommand.ExecuteNonQuery();
+
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new VoertuigException($"Er ging iets mis in de persitentielaag bij het updaten van het voertuig, { ex.Message}");
+            }
+        }
+
+        public void DeleteVoertuig(Voertuig voertuig)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string deleteSql = "UPDATE voertuig SET isDeleted = 1 WHERE chassisnummer = @Chassisnummer";
+                    SqlCommand deleteCommand = new(deleteSql, conn);
+
+                    deleteCommand.Parameters.AddWithValue("@Chassisnummer", voertuig.Chassisnummer);
+
+                    deleteCommand.ExecuteNonQuery();
+
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new VoertuigException($"Er ging iets in de persitentielaag mis bij het verwijderen van het voertuig" +
+                    $" met chassisnummer: {voertuig.Chassisnummer}, {ex.Message}");
+            }
+        }
+
+        public void RetrieveVoertuig(Voertuig voertuig)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string deleteSql = "UPDATE voertuig SET isDeleted = 0 WHERE chassisnummer = @Chassisnummer";
+                    SqlCommand deleteCommand = new(deleteSql, conn);
+
+                    deleteCommand.Parameters.AddWithValue("@Chassisnummer", voertuig.Chassisnummer);
+
+                    deleteCommand.ExecuteNonQuery();
+
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new VoertuigException($"Er ging iets mis in de persitentielaag bij het retrieven van het voertuig " +
+                    $"met chassisnummer: {voertuig.Chassisnummer}, {ex.Message}");
+            }
         }
     }
 }
